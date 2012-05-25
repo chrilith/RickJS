532e2706adee3b2eb763a79712fa8c94c53bb313
Chris Apers <chrilith@hotmail.com>
2012-05-25 11:41:43 +0200

    Sound loading is not fatal, also use WAV sound for better support

--- source/xrick.js
+++ source/xrick.js
@@ -36,7 +36,7 @@ XRick = {
 		var nb = 0,
 			complete = function() {
 
-			if (++nb == 29) {
+			if (++nb == 5) {
 				Tiles.data = [];
 				Tiles.data[0] = new G.TileSet(Data.getItem("tiles_data"), new G.Rect(0, 0 * 8 * 256, 8, 8 * 256), 256, 8, 8);
 				Tiles.data[1] = new G.TileSet(Data.getItem("tiles_data"), new G.Rect(0, 1 * 8 * 256, 8, 8 * 256), 256, 8, 8);
@@ -49,7 +49,7 @@ XRick = {
 		}
 		
 		Data = new G.BitmapLibrary(complete);
-		Snd = new G.SoundPool(complete);
+		Snd = new G.SoundPool(new Function);
 
 		Data.loadItem("pic_splash", "data/title.png");
 		Data.loadItem("pic_haf", "data/haf.png");
@@ -57,37 +57,36 @@ XRick = {
 		Data.loadItem("tiles_data", "data/tiles.png");
 		Data.loadItem("sprites_data", "data/sprites.png");
 
+		Snd.loadItem("WAV_GAMEOVER", "sound/gameover.wav");
+		Snd.loadItem("WAV_SBONUS2", "sound/sbonus2.wav");
+		Snd.loadItem("WAV_BULLET", "sound/bullet.wav");
+		Snd.loadItem("WAV_BOMBSHHT", "sound/bombshht.wav");
+		Snd.loadItem("WAV_EXPLODE", "sound/explode.wav");
+		Snd.loadItem("WAV_STICK", "sound/stick.wav");
+		Snd.loadItem("WAV_WALK", "sound/walk.wav");
+		Snd.loadItem("WAV_CRAWL", "sound/crawl.wav");
+		Snd.loadItem("WAV_JUMP", "sound/jump.wav");
+		Snd.loadItem("WAV_PAD", "sound/pad.wav");
+		Snd.loadItem("WAV_BOX", "sound/box.wav");
+		Snd.loadItem("WAV_BONUS", "sound/bonus.wav");
+		Snd.loadItem("WAV_SBONUS1", "sound/sbonus1.wav");
+		Snd.loadItem("WAV_DIE", "sound/die.wav");
+		Snd.loadItem("WAV_ENTITY0", "sound/ent0.wav");
+		Snd.loadItem("WAV_ENTITY1", "sound/ent1.wav");
+		Snd.loadItem("WAV_ENTITY2", "sound/ent2.wav");
+		Snd.loadItem("WAV_ENTITY3", "sound/ent3.wav");
+		Snd.loadItem("WAV_ENTITY4", "sound/ent4.wav");
+		Snd.loadItem("WAV_ENTITY5", "sound/ent5.wav");
+		Snd.loadItem("WAV_ENTITY6", "sound/ent6.wav");
+		Snd.loadItem("WAV_ENTITY7", "sound/ent7.wav");
+		Snd.loadItem("WAV_ENTITY8", "sound/ent8.wav");
 
-		Snd.loadItem("WAV_GAMEOVER", "sound/gameover.mp3");
-		Snd.loadItem("WAV_SBONUS2", "sound/sbonus2.mp3");
-		Snd.loadItem("WAV_BULLET", "sound/bullet.mp3");
-		Snd.loadItem("WAV_BOMBSHHT", "sound/bombshht.mp3");
-		Snd.loadItem("WAV_EXPLODE", "sound/explode.mp3");
-		Snd.loadItem("WAV_STICK", "sound/stick.mp3");
-		Snd.loadItem("WAV_WALK", "sound/walk.mp3");
-		Snd.loadItem("WAV_CRAWL", "sound/crawl.mp3");
-		Snd.loadItem("WAV_JUMP", "sound/jump.mp3");
-		Snd.loadItem("WAV_PAD", "sound/pad.mp3");
-		Snd.loadItem("WAV_BOX", "sound/box.mp3");
-		Snd.loadItem("WAV_BONUS", "sound/bonus.mp3");
-		Snd.loadItem("WAV_SBONUS1", "sound/sbonus1.mp3");
-		Snd.loadItem("WAV_DIE", "sound/die.mp3");
-		Snd.loadItem("WAV_ENTITY0", "sound/ent0.mp3");
-		Snd.loadItem("WAV_ENTITY1", "sound/ent1.mp3");
-		Snd.loadItem("WAV_ENTITY2", "sound/ent2.mp3");
-		Snd.loadItem("WAV_ENTITY3", "sound/ent3.mp3");
-		Snd.loadItem("WAV_ENTITY4", "sound/ent4.mp3");
-		Snd.loadItem("WAV_ENTITY5", "sound/ent5.mp3");
-		Snd.loadItem("WAV_ENTITY6", "sound/ent6.mp3");
-		Snd.loadItem("WAV_ENTITY7", "sound/ent7.mp3");
-		Snd.loadItem("WAV_ENTITY8", "sound/ent8.mp3");
-
-		Snd.loadItem("WAV_TUNE0", "sound/tune0.mp3");
-		Snd.loadItem("WAV_TUNE1", "sound/tune1.mp3");
-		Snd.loadItem("WAV_TUNE2", "sound/tune2.mp3");
-		Snd.loadItem("WAV_TUNE3", "sound/tune3.mp3");
-		Snd.loadItem("WAV_TUNE4", "sound/tune4.mp3");
-		Snd.loadItem("WAV_TUNE5", "sound/tune5.mp3");
+		Snd.loadItem("WAV_TUNE0", "sound/tune0.wav");
+		Snd.loadItem("WAV_TUNE1", "sound/tune1.wav");
+		Snd.loadItem("WAV_TUNE2", "sound/tune2.wav");
+		Snd.loadItem("WAV_TUNE3", "sound/tune3.wav");
+		Snd.loadItem("WAV_TUNE4", "sound/tune4.wav");
+		Snd.loadItem("WAV_TUNE5", "sound/tune5.wav");
 
 		Sysarg.args_map = 0;
 		Sysarg.args_submap = 0;
